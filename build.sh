0fe406565aaa87bfe5127533bd2dfc0c7e5615ca
Jhordy Gavinchu <65508346+jhordyess@users.noreply.github.com>
2022-09-11 14:03:58 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -av ./src/ ./build/
-cp ./pom.xml ./build/
+cp -v ./pom.xml ./build/
 cd build
 docker compose -p orders-man-java -f ./docker-compose.yml up -d
-rm -rf ./src ./pom.xml
+rm -rv ./src ./pom.xml
